681de3cf2ad6b963c00daec9d4e53879f3d5ff13
Azanul <azanulhaque@gmail.com>
2023-05-18 17:50:16 +0530

    refac
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- rust/D1-CalorieCounting/v1/src/main.rs
+++ rust/D1-CalorieCounting/v1/src/main.rs
@@ -3,16 +3,15 @@ use std::fs;
 fn main() {
     let file_contents = fs::read_to_string("../input.txt").unwrap();
 
-    println!(
-        "{}",
-        file_contents
-            .split("\n\n")
-            .map(|x| {
-                x.split("\n")
-                    .map(|element| element.parse().unwrap_or(0))
-                    .sum::<i32>()
-            })
-            .max()
-            .unwrap()
-    );
+    let max_sum = file_contents
+        .split("\n\n")
+        .map(|x| {
+            x.split("\n")
+                .map(|element| element.parse().unwrap_or(0))
+                .sum::<i32>()
+        })
+        .max()
+        .unwrap();
+
+    println!("{}", max_sum)
 }
